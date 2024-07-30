1155714804cf7dbefec30786edd80ab0ecf93877
rahulabh-droid <135652436+rahulabh-droid@users.noreply.github.com>
2024-07-30 15:37:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var Randomnumber1 = Math.floor(Math.random()*6)+1;
+
+var Randomdiceimage = "dice" + Randomnumber1 + ".png";
+
+var RandomImageSource = "images/" + Randomdiceimage;
+
+img1.setAttribute("src", RandomImageSource);
+
+
+
+
+
+
